29cdbe37e489fbd1d3ead578e03e4550bc86e1cc
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2023-10-04 13:56:57 +0200

    useFetch hook added

---
+++ src/custom-hooks/useFetch/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(
+            "Failed to retrieve the data for the requested resource."
+          );
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
+      });
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
---
+++ src/custom-hooks/useFetch/useFetchExample.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import useFetch from './useFetch';
+
+const useFetchExample = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default useFetchExample;
