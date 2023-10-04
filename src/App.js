0edff2a53d1ae8a83122e8ad87b22c53c14d80c3
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2023-10-04 14:39:34 +0200

    instructions added if someone want to try the useFetch custom hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import FetchExample from "./custom-hooks/useFetch/FetchExample";
+// import FetchExample from "./custom-hooks/useFetch/FetchExample";
 import Container from "./container/Container";
 
 function App() {
   return (
     <React.Fragment>
       <Container>
-        <FetchExample />
+        {/* If u want to try useFetch hook feel free to uncomment component below, and also uncomment Fetchexample import from above */}
+        {/* <FetchExample /> */}
       </Container>
     </React.Fragment>
   );
